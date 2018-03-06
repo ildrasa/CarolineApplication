3ba1688f77fc6afb1c29d2042401928b4efda37f
ildrasa <ildrasa@kalash-PC.home>
2018-03-07 00:36:00 +0100

    [add] add rest requester

---
+++ src/main/java/com/cgir/caroline/RestRequester.java
@@ -0,0 +1,49 @@
+package com.cgir.caroline;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RestRequester {
+
+	// example: target = "http://localhost:8080/RESTfulExample/json/product/get"
+	public String getRequest(String target) {
+
+		StringBuilder result = new StringBuilder();
+		try {
+
+			URL url = new URL(target);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output;
+
+			while ((output = br.readLine()) != null) {
+				result.append(output);
+			}
+
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+		return result.toString();
+	}
+}
