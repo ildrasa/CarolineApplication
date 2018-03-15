37063840af74bf6045f2cbdbbc8d8a2c3f593db2
ildrasa <ildrasa@kalash-PC.home>
2018-03-15 22:27:19 +0100

    [add] add properties files

--- src/main/java/com/cgir/caroline/Caroline.java
+++ src/main/java/com/cgir/caroline/Caroline.java
@@ -1,32 +1,53 @@
 package com.cgir.caroline;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Caroline {
 
-	String ALICE_URL = "http://127.0.0.1:5001/alice/rest/alice";
-	String BRUNEHILDE_URL = "http://127.0.0.1:5002/";
+	private Properties _carolineProperties;
 
 	private RestRequester _restRequester;
-
-	public Caroline() {
-		_restRequester = new RestRequester();
-	}
 
 	/**
 	 * constructor for tests
 	 * 
+	 * @param carolineProperties
+	 * 
 	 * @param _restRequesterMock
 	 */
-	Caroline(RestRequester restRequester) {
+	Caroline(RestRequester restRequester, Properties carolineProperties) {
 		_restRequester = restRequester;
+		_carolineProperties = carolineProperties;
+	}
+
+	public Caroline() {
+		RestRequester restRequester = new RestRequester();
+		FileInputStream in;
+		Properties carolineProperties = new Properties();
+		try {
+			in = new FileInputStream("./src/conf/caroline-conf.properties");
+			carolineProperties.load(in);
+			in.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		new Caroline(restRequester, carolineProperties);
 	}
 
 	public String getAliceVersion() {
-		String aliceVersion = _restRequester.getRequest(ALICE_URL + "/version");
+		String aliceVersion = _restRequester.getRequest(_carolineProperties.getProperty("ALICE_URL") + "/version");
 		return aliceVersion;
 	}
 
 	public String getBrunehildeText() {
-		String brunehildeText = _restRequester.getRequest(BRUNEHILDE_URL);
+		String brunehildeText = _restRequester.getRequest(_carolineProperties.getProperty("BRUNEHILDE_URL"));
 		return brunehildeText;
 	}
 }
--- src/test/java/com/cgir/caroline/CarolineTest.java
+++ src/test/java/com/cgir/caroline/CarolineTest.java
@@ -1,5 +1,10 @@
 package com.cgir.caroline;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,11 +17,13 @@ public class CarolineTest {
 
 	private RestRequester _restRequesterMock;
 	private Caroline _caroline;
+	private Properties _propertiesMock;
 
 	@Before
 	public void init() {
 		_restRequesterMock = Mockito.mock(RestRequester.class);
-		_caroline = new Caroline(_restRequesterMock);
+		_propertiesMock = Mockito.mock(Properties.class);
+		_caroline = new Caroline(_restRequesterMock,_propertiesMock);
 	}
 
 	@Test
@@ -25,6 +32,7 @@ public class CarolineTest {
 
 		String aliceVersion = _caroline.getAliceVersion();
 
+		Mockito.verify(_propertiesMock).getProperty("ALICE_URL");
 		Mockito.verify(_restRequesterMock, Mockito.times(1)).getRequest(Matchers.endsWith("/version"));
 		Assert.assertEquals("1.0", aliceVersion);
 	}
@@ -35,6 +43,7 @@ public class CarolineTest {
 
 		String brunehildeText = _caroline.getBrunehildeText();
 
+		Mockito.verify(_propertiesMock).getProperty("BRUNEHILDE_URL");
 		Mockito.verify(_restRequesterMock, Mockito.times(1)).getRequest(Matchers.anyString());
 		Assert.assertEquals("text", brunehildeText);
 	}
